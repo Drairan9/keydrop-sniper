9b0ef0df043d431e83ce976812fb28b3c91ba705
Drairan9 <drairan@protonmail.com>
2023-04-14 21:30:48 +0200

    frontend pt1

--- test.js
+++ test.js
@@ -64,35 +64,3 @@ async function startBattleListener() {
         });
     }
 }
-
-async function startBattleListenerBACK() {
-    if (listenerSettings.called) return;
-    listenerSettings.called = true;
-
-    while (listenerSettings.listening) {
-        await new Promise((resolve, reject) => setTimeout(resolve, 100));
-        console.log(`Searching battle with ${listenerSettings.players} players.`);
-        const battles = Array.from(document.querySelectorAll('.relative.my-10.z-0')).filter((battle) =>
-            battle.querySelector('.text-center.text-sm.font-bold.text-green')
-        );
-        battles.forEach((item) => {
-            let casesElements = item.querySelectorAll(
-                '.line-clamp.max-w-full.overflow-hidden.break-words.px-1.text-center.font-semibold.leading-none.text-white'
-            );
-            let playersAmount = item.querySelector('.ml-4.text-base.font-bold.text-white')?.textContent;
-            if (playersAmount === undefined) return;
-
-            playersAmount = parseInt(playersAmount.split('/')[1]);
-
-            let casesArray = Array.from(casesElements, (p) => p.textContent);
-            if (casesArray.find((name) => TARGETS.includes(name))) {
-                let button = item.querySelector('.button.ml-1.mr-5.h-12.w-auto.flex-1.button-green-dimmed');
-                if (button) {
-                    button.click();
-                    listenerSettings.listening = false;
-                    listenerSettings.called = false;
-                }
-            }
-        });
-    }
-}
