e60da6442af0dee87c5cbb9e3b6b1a7bd0a03858
Drairan9 <drairan@protonmail.com>
2023-04-19 10:22:48 +0200

    updated icons

--- background/contentController.js
+++ background/contentController.js
@@ -2,6 +2,8 @@ const startButton = document.querySelector('.join-button');
 
 startButton.addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'start-listening' });
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'start-listening' }).catch((err) => {
+            console.log(err);
+        });
     });
 });
