7c419f1d2564f8df8efc1b83e8df08e85087720e
Drairan9 <drairan@protonmail.com>
2023-04-19 10:38:37 +0200

    passing user options to content script

--- background/contentController.js
+++ background/contentController.js
@@ -1,9 +1,21 @@
 const startButton = document.querySelector('.join-button');
 
 startButton.addEventListener('click', () => {
+    let selectedCases = [];
+    let selectedPlayers = [];
+
+    document.querySelectorAll('.case-checkbox').forEach((element) => {
+        if (element.checked) selectedCases.push(element.dataset.caseName.toUpperCase());
+    });
+    document.querySelectorAll('.players-checkbox').forEach((element) => {
+        if (element.checked) selectedPlayers.push(element.dataset.number);
+    });
+
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'start-listening' }).catch((err) => {
-            console.log(err);
-        });
+        chrome.tabs
+            .sendMessage(tabs[0].id, { action: 'start-listening', cases: selectedCases, players: selectedPlayers })
+            .catch((err) => {
+                console.log(err);
+            });
     });
 });
--- content_scripts/contentScript.js
+++ content_scripts/contentScript.js
@@ -1,9 +1,35 @@
 console.log('%c key-sniper injected', 'color: red;');
 
+var listenerSettings = {
+    listening: false,
+    called: false,
+    players: [],
+    cases: ['TOXIC', 'DIABLO', 'MILSPEC', 'SPARK', 'DAGGER', 'ENERGY', 'ICE BLAST', 'ROCKET RACCON', 'TECH', 'DAGGERS'],
+};
+const TARGETS = [
+    'TOXIC',
+    'DIABLO',
+    'MILSPEC',
+    'SPARK',
+    'DAGGER',
+    'ENERGY',
+    'ICE BLAST',
+    'ROCKET RACCON',
+    'TECH',
+    'DAGGERS',
+    'CERES',
+    'TECH',
+    'JOY',
+    'PROGRESS',
+];
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.action == 'start-listening') {
-        console.log('creating');
-        createIsland();
+        listenerSettings.listening = true;
+        listenerSettings.players = message.players.length > 0 ? message.players : [2, 3, 4];
+        listenerSettings.cases = message.cases.length > 0 ? message.cases : listenerSettings.cases;
+        createIsland(listenerSettings.players, listenerSettings.cases);
+        startBattleListener();
     }
 });
 
@@ -34,6 +60,39 @@ function createIsland(players = [0], cases = ['NONE', 'TECH', 'ICE BLAST', 'CERE
     document.body.appendChild(container);
 }
 
+async function startBattleListener() {
+    if (listenerSettings.called) return;
+    listenerSettings.called = true;
+
+    while (listenerSettings.listening) {
+        await new Promise((resolve, reject) => setTimeout(resolve, 50));
+        console.log(`Searching battle with ${listenerSettings.players} players.`);
+        const battles = Array.from(document.querySelectorAll('.relative.my-10.z-0')).filter((battle) =>
+            battle.querySelector('.text-center.text-sm.font-bold.text-green')
+        );
+        battles.forEach((item) => {
+            let casesElements = item.querySelectorAll(
+                '.line-clamp.max-w-full.overflow-hidden.break-words.px-1.text-center.font-semibold.leading-none.text-white'
+            );
+            let playersAmount = item.querySelector('.ml-4.text-base.font-bold.text-white')?.textContent;
+            if (playersAmount === undefined) return;
+
+            playersAmount = parseInt(playersAmount.split('/')[1]);
+            if (!listenerSettings.players.includes(playersAmount)) return;
+
+            let casesArray = Array.from(casesElements, (p) => p.textContent);
+            if (casesArray.find((name) => TARGETS.includes(name))) {
+                let button = item.querySelector('.button.ml-1.mr-5.h-12.w-auto.flex-1.button-green-dimmed');
+                if (button) {
+                    button.click();
+                    listenerSettings.listening = false;
+                    listenerSettings.called = false;
+                }
+            }
+        });
+    }
+}
+
 function _createElement(tagName, attributes, textContent) {
     const element = document.createElement(tagName);
 
@@ -56,60 +115,3 @@ function _appendChilds(parent, children) {
         parent.appendChild(element);
     });
 }
-
-// var listenerSettings = {
-//     listening: false,
-//     called: false,
-//     players: [],
-//     cases: ['TOXIC', 'DIABLO', 'MILSPEC', 'SPARK', 'DAGGER', 'ENERGY', 'ICE BLAST', 'ROCKET RACCON', 'TECH', 'DAGGERS'],
-// };
-
-// const TARGETS = [
-//     'TOXIC',
-//     'DIABLO',
-//     'MILSPEC',
-//     'SPARK',
-//     'DAGGER',
-//     'ENERGY',
-//     'ICE BLAST',
-//     'ROCKET RACCON',
-//     'TECH',
-//     'DAGGERS',
-//     'CERES',
-//     'TECH',
-//     'JOY',
-//     'PROGRESS',
-// ];
-
-// async function startBattleListener() {
-//     if (listenerSettings.called) return;
-//     listenerSettings.called = true;
-
-//     while (listenerSettings.listening) {
-//         await new Promise((resolve, reject) => setTimeout(resolve, 50));
-//         console.log(`Searching battle with ${listenerSettings.players} players.`);
-//         const battles = Array.from(document.querySelectorAll('.relative.my-10.z-0')).filter((battle) =>
-//             battle.querySelector('.text-center.text-sm.font-bold.text-green')
-//         );
-//         battles.forEach((item) => {
-//             let casesElements = item.querySelectorAll(
-//                 '.line-clamp.max-w-full.overflow-hidden.break-words.px-1.text-center.font-semibold.leading-none.text-white'
-//             );
-//             let playersAmount = item.querySelector('.ml-4.text-base.font-bold.text-white')?.textContent;
-//             if (playersAmount === undefined) return;
-
-//             playersAmount = parseInt(playersAmount.split('/')[1]);
-//             if (!listenerSettings.players.includes(playersAmount)) return;
-
-//             let casesArray = Array.from(casesElements, (p) => p.textContent);
-//             if (casesArray.find((name) => TARGETS.includes(name))) {
-//                 let button = item.querySelector('.button.ml-1.mr-5.h-12.w-auto.flex-1.button-green-dimmed');
-//                 if (button) {
-//                     button.click();
-//                     listenerSettings.listening = false;
-//                     listenerSettings.called = false;
-//                 }
-//             }
-//         });
-//     }
-// }
